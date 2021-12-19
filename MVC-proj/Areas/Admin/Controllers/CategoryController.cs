f902d610b135414da4276f0baf641207f3032608
vasif-student <vasifah@code.edu.az>
2021-12-19 22:27:53 +0400

    admin-panel

---
+++ MVC-proj/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_proj.DAL;
+using MVC_proj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_proj.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+    }
+}
---
+++ MVC-proj/Areas/Admin/Controllers/LayoutController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_proj.DAL;
+using MVC_proj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_proj.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LayoutController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public LayoutController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var layouts = await _context.Layouts.ToListAsync();
+            return View(layouts);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var layout = await _context.Layouts.FindAsync(id);
+            if(layout == null)
+            {
+                return NotFound();
+            }
+
+            return View(layout);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Layout layout)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _context.Layouts.AddAsync(layout);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
